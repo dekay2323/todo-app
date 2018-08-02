3b0d709519022679ce14efc3b5ea6c1cd517ed86
dekay2323 <dekay.2323@gmail.com>
2018-08-01 18:14:29 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <TodoList v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo AA',
+        projects: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo AA',
+        projects: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo AA',
+        projects: 'Project A',
+        done: false,
+      }],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
